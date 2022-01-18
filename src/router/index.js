36733ebc6a63a942f8d0d511a9f500b9a9713d53
svetlana <developer-svetlana@yandex.ru>
2022-01-18 17:27:47 +0500

    dz6

---
+++ src/router/index.js
@@ -0,0 +1,66 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: () =>
+      import(/* webpackChunkName: "Dashboard" */ "../views/Dashboard.vue"),
+  },
+  {
+    path: "/dashboard/:page",
+    name: "dashboard",
+    component: () =>
+      import(/* webpackChunkName: "Dashboard" */ "../views/Dashboard.vue"),
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () =>
+      import(/* webpackChunkName: "About" */ "../views/About.vue"),
+  },
+  {
+    path: "/notfound",
+    name: "notfound",
+    component: () =>
+      import(/* webpackChunkName: "NotFound" */ "../views/NotFound.vue"),
+  },
+  {
+    path: "*",
+    component: () =>
+      import(/* webpackChunkName: "NotFound" */ "../views/NotFound.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes,
+});
+
+const userAuth = true;
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== "notfound" && !userAuth) {
+    next({
+      name: "notfound",
+    });
+  } else {
+    next();
+  }
+});
+
+const getTitleByRoutes = (routeName) => {
+  return {
+    dashboard: "Страница трат",
+    about: "Страница о сервисе",
+    notfound: "Страница не найдена",
+  }[routeName];
+};
+
+router.afterEach((to) => {
+  document.title = getTitleByRoutes(to.name);
+});
+
+export default router;
