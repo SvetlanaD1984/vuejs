c32e9fb73b2ead0e8ec8da2f6f4c647fe3a4caab
svetlana <developer-svetlana@yandex.ru>
2022-01-18 15:57:19 +0500

    dz4new

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,75 @@
 <template>
   <div id="app">
-    <header class="header">
-      My personal costs
-    </header>
-    <main>
-     <PaymentForm @updateData="updateData" />
-     
-      <PaymentsList :items="paymentsList" />
-    </main>
+    <div class="wrapper">
+      <header>
+        <div class="title">My personal costs</div>
+
+        <div class="total" v-if="total">Total: {{ total }}</div>
+      </header>
+      <main>
+        <add-payment-form />
+    <payments-display :items="currentElements" />
+    <pagination :cur="page" :n="n" :length="12" @paginate="changePage" />
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import PaymentsList from './components/PaymentsList'
-import PaymentForm from './components/PaymentForm'
-import { mapActions } from 'vuex'
+import AddPaymentForm from "./components/AddPaymentForm.vue";
+import PaymentsDisplay from "./components/PaymentsDisplay.vue";
+import { mapMutations, mapGetters, mapActions } from "vuex";
+import Pagination from "./components/Pagination.vue";
 export default {
-  name: 'App',
-  components: {
-    PaymentsList,
-    PaymentForm
-  },
-  data () {
+  components: { PaymentsDisplay, AddPaymentForm, Pagination },
+  name: "App",
+  data() {
     return {
-    }
+      show: true,
+      page: 1,
+      n: 3,
+    };
+  },
+   computed: {
+    ...mapGetters(["getPaymentsList"]),
+    total() {
+      return this.$store.getters.getPaymentsListFullValuePrice;
+    },
+    currentElements() {
+      return this.getPaymentsList.slice(this.n * (this.page - 1), this.n * (this.page - 1) + this.n);
+    },
   },
   methods: {
-    // ...mapActions([
-    //   'fetchData'
-    // ]),
-    ...mapActions({
-      fetchListData: 'fetchData'
-    }),
-   /* onDataAdded (data) {
-      this.paymentsList.push(data)
-    },*/
-    
-  
+    ...mapMutations({ fetch: "setPaymentsListData" }),
+    ...mapActions(["fetchData"]),
+    changePage(p) {
+      this.page = p;
+      this.fetchData(p);
+    },
   },
-  mounted () {
-    this.fetchListData()
-  }
-}
+  async created() {
+    await this.fetchData();
+    if (this.$route.params?.page) {
+      this.page = Number(this.$route.params.page);
+    }
+    // this.$store.dispatch('fetchData');
+    // this.$store.commit('setPaymentsListData', this.fetchData());
+  },
+};
 </script>
 
-<style  >
-.header {
-  color: black;
+<style lang="scss" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  font-size: 20px;
 }
 </style>
+
+
--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,29 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export const localDB = {
+export const localStorage = {
  
   "page1": [
-	{ "id": 1, "date": "20.03.2020", "category": "Food", "value": 169 },
-	{ "id": 2, "date": "21.03.2020", "category": "Navigation", "value": 50 },
-	{ "id": 3, "date": "22.03.2020", "category": "Sport", "value": 450 }
+  { "id": 1, "date": "20.03.2020", "category": "Sport", "value": 169 },
+  { "id": 2, "date": "21.03.2020", "category": "Education", "value": 50 },
+  { "id": 3, "date": "22.03.2020", "category": "Internet", "value": 450 }
   ],
   "page2": [
-	{ "id": 4, "date": "23.03.2020", "category": "Entertaiment", "value": 969 },
-	{ "id": 5, "date": "24.03.2020", "category": "Education", "value": 1500 },
-	{ "id": 6, "date": "25.03.2020", "category": "Food", "value": 200 }
+  { "id": 4, "date": "23.03.2020", "category": "Sport", "value": 969 },
+  { "id": 5, "date": "24.03.2020", "category": "Transport", "value": 1500 },
+  { "id": 6, "date": "25.03.2020", "category": "Food", "value": 200 }
   ],
   "page3": [
-  { "id": 7, "date": "23.03.2020", "category": "Entertaiment", "value": 969 },
-  { "id": 8, "date": "24.03.2020", "category": "Education", "value": 1500 },
-  { "id": 9, "date": "25.03.2020", "category": "Food", "value": 200 }
+  { "id": 7, "date": "26.03.2020", "category": "Internet", "value": 969 },
+  { "id": 8, "date": "27.03.2020", "category": "Education", "value": 1500 },
+  { "id": 9, "date": "28.03.2020", "category": "Food", "value": 200 }
   ],
   "page4": [
-   { "id": 10, "date": "23.03.2020", "category": "Entertaiment", "value": 969 },
-   { "id": 11, "date": "24.03.2020", "category": "Education", "value": 1500 },
-   { "id": 12, "date": "25.03.2020", "category": "Food", "value": 200 }
+   { "id": 10, "date": "29.03.2020", "category": "Internet", "value": 969 },
+   { "id": 11, "date": "30.03.2020", "category": "Education", "value": 1500 },
+   { "id": 12, "date": "31.03.2020", "category": "Food", "value": 200 }
    ],
 }
 
@@ -31,73 +31,56 @@ export const localDB = {
 export default new Vuex.Store({
   state: {
     paymentsList: [],
-    paymentsListIDS: [],
-   
+    paymentsListIds: [],
+    categoryList: [],
   },
   mutations: {
-addNewPayment (state, payload) {
-  const id = state.paymentsListIDS.length > 12 ? state.paymentsListIDS.length : 13
-  state.paymentsListIDS.push(id)
-  payload.id = id
-  state.getPaymentsList.push(payload)
-  },
-
-updatePayment (state, payload) {
-  const item = state.getPaymentsList.find(item => item.id === payload)
-  if (item) {
-  Object.assign(item, payload)
-}
-},
-
-    setPaymentsListData (state, payload) {
-      state.paymentsList = payload
+    setPaymentsListData(state, payload) {
+      const newUniqIdsObs = payload.filter((item) => {
+        return state.paymentsListIds.indexOf(item.id) < 0
+      });
+      const uniqIds = newUniqIdsObs.map((obj) => obj.id);
+      state.paymentsList.push(...newUniqIdsObs);
+      state.paymentsListIds.push(...uniqIds);
     },
-   
-    addPaymentsListData (state, payload) {
-      const newUniqObjs = payload.filter(obj => {
-        return state.paymentsListIDS.indexOf(obj.id) < 0 
-      })
-      
-
-    const uniqIDS = newUniqObjs.map(obj => obj.id)
-    state.paymentsListIDS.push(...uniqIDS)
-      state.paymentsList.push(...newUniqObjs)
-      
-      
-  },
-  
-
-      
+    addPaymentListData(state, payload) {
+      state.paymentsList.push(payload);
+    },
+    editPaymentListFirstElement(state, payload) {
+      // state.paymentsList[0] = payload
+      Vue.set(state.paymentsList, 0, payload);
+      // OR
+      state.paymentsList[0] = payload;
+      state.paymentsList = [...state.paymentsList];
+    },
+    setCategoryData(state, paylaod) {
+      state.categoryList = paylaod;
+    },
   },
   getters: {
-    getPaymentsList: state => state.paymentsList,
-    getPaymentsListFullPrice: state => {
-      return state.paymentsList
-        .reduce((res, cur) => res + cur.price, 0)
+    getPaymentsListFullValuePrice: (state) => {
+      return state.paymentsList.reduce((acc, cur) => acc + cur.value, 0);
     },
-   // getPaymentForm: state => state.paymentForm,
-  
-  
+    getPaymentsList: (state) =>
+      state.paymentsList?.length ? state.paymentsList : [],
+    getCategoryList: state => state.categoryList,
   },
   actions: {
-    fetchData ({ commit }) {
+    fetchData({ commit }, page) {
       return new Promise((resolve, reject) => {
         setTimeout(() => {
-          const items = []
-          for (let i = 1; i < 105; i++) {
-            items.push({
-              date: '13.05.2021',
-              category: 'Education',
-              price: i
-            })
-          }
-          resolve(items)
-        }, 2000)
-      })
-        .then(res => {
-          commit('setPaymentsListData', res)
-        })
+          const items = localStorage[`page${page}`];
+          resolve(items);
+        }, 2000);
+      }).then((res) => commit("setPaymentsListData", res));
     },
-    
-  }
-})
+    fetchCategory({ commit }) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const items = ["Sport", "Education", "Internet", "Food", "Transport"];
+          resolve(items);
+        }, 2000);
+      }).then((res) => commit("setCategoryData", res));
+    },
+  },
+});
